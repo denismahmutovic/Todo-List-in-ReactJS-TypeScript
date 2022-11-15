cac650998fee80dcfa9eff662baeb77a180b535b
denismahmutovic <djdeny95@gmail.com>
2022-11-15 18:28:58 +0100

    s

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 
 const App:FC = ()=>{
   return(
-    <div className='app'>
+    <div className='App'>
       <div className='header'>
+        <div className='inputContainer'>
          <input type={"text"} placeholder="task " />
-         <input type={"number "} placeholder="D  m   eadline (in days) " />
-         <button>Add Task</button>
+         <input type={"number "} placeholder="Deadline (in days) " />
+         </div>  
+         <button>Add</button>
          </div>
       <div className='todoList'  ></div>
     </div>
